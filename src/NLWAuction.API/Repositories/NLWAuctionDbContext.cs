473b1b62f7baa7fc850b201c5cca89e9da4d5c08
Italo Roberto <italoir06@gmail.com>
2024-02-08 11:32:11 -0300

    Adicionar arquivos de projeto.

---
+++ src/NLWAuction.API/Repositories/NLWAuctionDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLWAuction.API.Entities;
+
+namespace NLWAuction.API.Repositories;
+
+public class NLWAuctionDbContext : DbContext
+{
+    public DbSet<Auction> Auctions { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(@"Data Source=C:\Users\italo\Downloads\leilaoDbNLW.db");
+    }
+}
---
+++ src/NLWAuction.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLWAuction.API.Entities;
+using NLWAuction.API.Repositories;
+
+namespace NLWAuction.API.UseCases.Auctions.GetCurrent;
+
+public class GetCurrentAuctionUseCase
+{
+    public Auction? Execute()
+    {
+        var repository = new NLWAuctionDbContext();
+
+        return repository
+            .Auctions
+            .Include(auction => auction.Items)
+            .FirstOrDefault();
+    }
+}
